d3395e7225f7b0bf7572168d814975597c4cee1b
bherrschaft <bherrschaft92994@gmail.com>
2024-07-01 16:38:21 -0400

    editss

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() { 
+   count += 1
+    countEl.innerText = count;
+    
+}
+
+function save() {
+   let countdash = count + " - "
+   saveEl.textContent += countdash;
+   console.log(count);
+}
+
 
---
+++ script1.js
@@ -0,0 +1,9 @@
+let welcomeEL = document.getElementById("welcome-el");
+
+let name = 'Brandon Herrschaft';
+
+let greeting = 'Hello my name is ' 
+
+welcomeEL.innerText = greeting + name; 
+
+welcomeEL.innerHTML += '👍'
